a358eea5413b160b6d772deebc7db580f156215c
Punxnot <do-you-speak@yandex.ru>
2019-04-14 04:19:56 +0300

    Return 10 most frequent words

--- server.js
+++ server.js
@@ -14,32 +14,76 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.raw());
 
+const sortByValue = (obj) => {
+  // Convert object into array
+	let sortable = [];
+
+	for(let key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      sortable.push([key, obj[key]]);
+    }
+  }
+
+	// Sort items by value
+	sortable.sort(function(a, b) {
+	  return b[1]-a[1]; // compare numbers
+	});
+
+	return sortable; // array in format [ [ key1, val1 ], [ key2, val2 ], ... ]
+};
+
+const wordsFrequency = (wordsArray) => {
+  let result = {};
+
+  for (let i=0; i<wordsArray.length; i++) {
+    if (result[wordsArray[i]]) {
+      result[wordsArray[i]]++;
+    } else {
+      result[wordsArray[i]] = 1;
+    }
+  }
+
+  return result;
+};
+
 const languageDiversity = (text) => {
   // Remove HTML tags from resulting string
   text = striptags(text).toLowerCase();
 
   // Create an array of all words
   let allWords = tokenizer.tokenize(text);
-  console.log("Before removing stopwords:");
-  console.log(allWords);
+  // console.log("Before removing stopwords:");
+  // console.log(allWords.length);
+
+  const customStopwords = ["и", "который", "которая", "которые", "которое", "которым", "которых", "все", "всё", "свой", "свои", "своя", "сам", "сами", "сама", "само", "такой", "такая", "такие", "такое", "ещё", "еще", "nbsp"];
 
   // Remove Russian stopwords
   let meaningfulWords = sw.removeStopwords(allWords, sw.ru);
-  console.log("After removing stopwords:");
-  console.log(meaningfulWords);
+
+  // Remove custom stopwords
+  meaningfulWords = sw.removeStopwords(meaningfulWords, customStopwords);
+  // console.log("After removing stopwords:");
+  // console.log(meaningfulWords.length);
 
   // Stem each word in the array
   const stemmedWords = meaningfulWords.map(x => natural.PorterStemmerRu.stem(x));
 
+  // Get 10 most frequent words
+  const wordsFrequencyObj = wordsFrequency(stemmedWords);
+  const mostFrequentWords = sortByValue(wordsFrequencyObj).slice(0, 10);
+  console.log(mostFrequentWords);
+
   // Remove duplicates
   const uniqueWords = Array.from(new Set(stemmedWords));
-  console.log("Unique words number:");
-  console.log(uniqueWords.length);
+  // console.log("Unique words number:");
+  // console.log(uniqueWords.length);
 
   // Calculate resulting text diversity
-  const diversity = uniqueWords.length / allWords.length;
+  // TODO: Maybe use allWords instead of meaningfulWords?
+  // TODO: Remove one-letter words
+  const diversity = uniqueWords.length / meaningfulWords.length;
 
-  return diversity;
+  return {"diversity": diversity, "mostFrequentWords": mostFrequentWords};
 };
 
 app.get('/posts/:user', (req, res) => {
@@ -70,7 +114,7 @@ app.get('/posts/:user', (req, res) => {
     }
 
     const result = {
-      diversity: languageDiversity(allPosts)
+      data: languageDiversity(allPosts)
     };
     console.log(JSON.stringify(result));
     console.log("================");
