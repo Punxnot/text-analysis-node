0d3d35b7432c6c9acffb3f3f64a7f2362dd26e04
Punxnot <do-you-speak@yandex.ru>
2019-05-04 00:40:06 +0300

    Add custom_stop_words.json

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 const app = express();
 const http = require('http').Server(app);
 const request = require('request');
@@ -9,13 +9,16 @@ const natural = require('natural');
 const tokenizer = new natural.AggressiveTokenizerRu({language: "ru"});
 const sw = require('stopword');
 const Morphy = require('phpmorphy').default;
+const customStopwords = require('./custom_stop_words.json').list;
+const decache = require('decache');
 
 const POSTSNUMBER = 25;
 const CHECKPOINT = 10000;
 
-const customStopwords = ["c","а","без","более","больше","большой","будем","будет","будете","будешь","буду","будут","будь","бы","бывает","был","была","были","было","быть","в","вам","вами","вас","ваш","ваша","ваше","ваши","ведь","весь","вне","во","вообще","вот","все","всем","всеми","всему","всех","всего","всею","всю","вся","всё","вы","г","где","да","даже","для","до","его","ее","если","еще","ещё","ею","её","же","за","зато","зачем","здесь","и","из","или","им","ими","их","к","каждая","каждое","каждые","каждый","как","какая","какие","какого","какой","какое","кем","когда","кого","ком","кому","конечно","которая","которого","которой","которое","которые","которым","который","которых","кроме","кто","куда","ли","лишь","меня","мимо","мне","много","мной","мною","мои","мой","моя","моё","мы","на","над","надо","наиболее","наконец","нам","нами","нас","наш","наша","наше","наши","не","него","нее","ней","нем","нему","нет","нею","неё","ни","нибудь","ним","ними","них","но","ну","нх","о","об","около","он","она","они","оно","от","очень","перед","по","под","пор","после","потому","почему","почти","при","про","сам","сама","сами","самим","самими","самих","само","самого","самой","самом","самому","саму","самый","свое","своём","своё","своего","своей","своему","свои","своя","своими","своих","свой","свою","своею","себе","себя","сих","со","собой","собою","та","так","такая","также","таки","такие","такое","такой","там","твои","твой","твоя","твоё","те","тебе","тебя","тем","теми","тех","то","тобой","тобою","того","тоже","только","том","тому","тот","тою","ту","туда","тут","ты","уж","уже","хоть","хотя","чего","чем","чему","через","что","чтоб","чтобы","чуть","эта","эти","этим","этими","этих","это","этого","этой","этом","этому","этот","эту","nbsp", "mdash", "ndash", "quot", "laquo", "raquo", "hellip"];
-
 const easterNames = ["mozgosteb", "bearinbloodbath", "adscripta"];
+
+// Invalidate cached json
+decache('./custom_stop_words.json');
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
@@ -127,7 +130,18 @@ const languageDiversity = (text, name) => {
 		diversity = (diversity - correction).toFixed(3);
 	}
 
-  return {"diversity": diversity, "mostFrequentWords": mostFrequentWords, "isEaster": isEaster, "averagePostLength": averagePostLength, "name": name};
+	let frequentWords = [];
+	for (let i=0; i<mostFrequentWords.length; i++) {
+		frequentWords.push(mostFrequentWords[i][0]);
+	}
+
+  return {
+    "diversity": diversity,
+    "mostFrequentWords": mostFrequentWords,
+    "isEaster": isEaster,
+    "averagePostLength": averagePostLength,
+    "name": name
+  };
 };
 
 app.get('/posts/:user', (req, res) => {
@@ -146,6 +160,7 @@ app.get('/posts/:user', (req, res) => {
     if (err) {
 			console.log("Something went wrong!")
       console.log(err);
+      res.status(404).send("Not found");
       return;
     } else if (response.statusCode == "404") {
 			res.status(404).send("Not found");
